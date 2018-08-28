f761d84dd1a55f73f45f9ae768bca18988e1b499
Mårten Hakkestad <marha721@student.liu.se>
2018-08-28 13:40:11 +0200

    Setup

---
+++ cpp-labs/cpp-labs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp-labs
+//
+//  Created by Mårten Hakkestad on 2018-08-28.
+//  Copyright © 2018 Mårten Hakkestad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
